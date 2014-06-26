3faa6b944274e0b45c8e379b85d39e9cb60d3c91
npal <nick.palladinos@gmail.com>
2014-06-26 18:54:50 +0300

    Basic code structure for Seq Streams

--- src/Streams.Core/Script.fsx
+++ src/Streams.Core/Script.fsx
@@ -1,1 +1,35 @@
-﻿
+﻿#time
+
+#load "Streams.fs"
+open Streams.Core
+
+
+let data = [|1..10000000|] |> Array.map int64
+
+data
+|> Seq.filter (fun x -> x % 2L = 0L)
+|> Seq.map (fun x -> x + 1L)
+|> Seq.sum
+
+
+data
+|> Array.filter (fun x -> x % 2L = 0L)
+|> Array.map (fun x -> x + 1L)
+|> Array.sum
+
+
+data
+|> Stream.ofArray
+|> Stream.filter (fun x -> x % 2L = 0L)
+|> Stream.map (fun x -> x + 1L)
+|> Stream.sum
+
+let baseline () = 
+    let mutable acc = 0L
+    for i = 0 to data.Length - 1 do
+        let x = data.[i]
+        if x % 2L = 0L then
+            acc <- acc + (x + 1L)
+    acc
+
+baseline()
---
+++ src/Streams.Core/Streams.fs
@@ -0,0 +1,47 @@
+﻿namespace Streams.Core
+
+module Stream =
+    
+    type Stream<'T> = Stream of (('T -> unit) -> unit)
+        
+    // generator functions
+    let inline ofArray (values : 'T []) : Stream<'T> =
+        Stream (fun iterf -> 
+                    for i = 0 to values.Length - 1 do
+                        iterf values.[i])
+
+    let inline ofSeq (values : seq<'T>) : Stream<'T> =
+        Stream (fun iterf -> 
+                    for value in values do
+                        iterf value)
+
+    // intermediate functions
+    let inline map (f : 'T -> 'R) (stream : Stream<'T>) : Stream<'R> =
+        Stream (fun iterf -> 
+            let (Stream streamf) = stream 
+            streamf (fun value -> iterf (f value)))
+
+    let inline flatmap (f : 'T -> Stream<'R>) (stream : Stream<'T>) : Stream<'R> =
+        Stream (fun iterf -> 
+            let (Stream streamf) = stream 
+            streamf (fun value -> 
+                        let (Stream streamf') = f value 
+                        streamf' iterf))
+
+    let inline collect (f : 'T -> Stream<'R>) (stream : Stream<'T>) : Stream<'R> =
+        flatmap f stream
+
+    let inline filter (p : 'T -> bool) (stream : Stream<'T>) : Stream<'T> =
+        Stream (fun iterf -> 
+            let (Stream streamf) = stream 
+            streamf (fun value -> if p value then iterf value))
+        
+    // terminal functions
+    let inline reduce (reducef : 'T -> 'R -> 'R) (init : 'R) (stream : Stream<'T>) : 'R = 
+        let accRef = ref init
+        let (Stream streamf) = stream 
+        streamf (fun value -> accRef := reducef value !accRef)
+        !accRef
+
+    let inline sum (stream : Stream<int64>) : int64 = 
+        reduce (+) 0L stream
