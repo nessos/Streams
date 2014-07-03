ba3e5b3c83a4242729292c751a49fda060e720cc
npal <nick.palladinos@gmail.com>
2014-07-03 19:57:58 +0300

    Program main

---
+++ tests/Streams.Tests/Program.fs
@@ -0,0 +1,17 @@
+﻿namespace Nessos.Streams.Tests
+open Nessos.Streams.Core
+
+module Program = 
+
+
+    [<EntryPoint>]
+    let main argv = 
+        let data = [|1..1000000|] |> Array.map int64
+        let result = 
+            data
+            |> Stream.ofArray
+            |> Stream.filter (fun x -> x % 2L = 0L)
+            |> Stream.map (fun x -> x + 1L)
+            |> Stream.sum
+        0
+
