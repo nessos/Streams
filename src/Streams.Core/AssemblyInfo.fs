d99040d8597bf10963acb90e1d7395742a29f869
krontogiannis <rodoconst@hotmail.com>
2014-07-07 18:06:29 +0300

    nunit.runners/assemblyinfo fix

---
+++ src/Streams.Core/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Streams")>]
+[<assembly: AssemblyProductAttribute("Streams")>]
+[<assembly: AssemblyCompanyAttribute("Nessos Information Technologies")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
