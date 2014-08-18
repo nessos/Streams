3da14f85169b589e4657eb02885faa2a38170ec6
npal <nick.palladinos@gmail.com>
2014-08-18 16:20:07 +0300

    Updated Streams.nuspec

--- src/Streams.Core/AssemblyInfo.fs
+++ src/Streams.Core/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Streams")>]
 [<assembly: AssemblyProductAttribute("Streams")>]
 [<assembly: AssemblyCompanyAttribute("Nessos Information Technologies")>]
-[<assembly: AssemblyVersionAttribute("0.1.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.5")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.5"
+    let [<Literal>] Version = "0.2.0"
