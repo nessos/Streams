5b391d2a2c4f2370bdd37fa63876528b9f120067
npal <nick.palladinos@gmail.com>
2014-08-27 20:12:11 +0300

    New CloudStream test project

--- tests/Streams.Cloud.Tests/Script.fsx
+++ tests/Streams.Cloud.Tests/Script.fsx
@@ -1,8 +1,21 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
+﻿
+#time
 
-#load "Library1.fs"
-open Streams.Cloud.Tests
 
-// Define your library scripting code here
+#r "bin/Debug/MBrace.Core.dll"
+#r "bin/Debug/MBrace.Lib.dll"
+//#load "../../packages/MBrace.Runtime.0.5.4-alpha/bootstrap.fsx" 
+#r "bin/Debug/Streams.Cloud.dll"
+
+open Nessos.Streams.Cloud
+open Nessos.MBrace
+
+let data = [|1..10000000|]
+
+
+data
+|> CloudStream.ofArray 
+|> CloudStream.filter (fun x -> x % 2 = 0)
+|> CloudStream.length
+
 
