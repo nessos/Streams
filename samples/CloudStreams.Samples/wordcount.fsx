151fd0a0348a0610877e56f8ee6511b5dcf14a09
krontogiannis <rodoconst@hotmail.com>
2014-09-26 17:57:33 +0300

    Added samples

---
+++ samples/CloudStreams.Samples/wordcount.fsx
@@ -0,0 +1,58 @@
+﻿
+#load "../../packages/MBrace.Runtime.0.5.7-alpha/bootstrap.fsx" 
+#r "bin/Debug/Streams.Core.dll"
+#r "bin/Debug/Streams.Cloud.dll"
+
+open System
+open System.IO
+open Nessos.MBrace
+open Nessos.MBrace.Client
+open Nessos.Streams.Core
+open Nessos.Streams.Cloud
+
+let path = "path to your files"
+let files = Directory.GetFiles(path)
+
+/// words ignored by wordcount
+let noiseWords = 
+    set [
+        "a"; "about"; "above"; "all"; "along"; "also"; "although"; "am"; "an"; "any"; "are"; "aren't"; "as"; "at";
+        "be"; "because"; "been"; "but"; "by"; "can"; "cannot"; "could"; "couldn't"; "did"; "didn't"; "do"; "does"; 
+        "doesn't"; "e.g."; "either"; "etc"; "etc."; "even"; "ever";"for"; "from"; "further"; "get"; "gets"; "got"; 
+        "had"; "hardly"; "has"; "hasn't"; "having"; "he"; "hence"; "her"; "here"; "hereby"; "herein"; "hereof"; 
+        "hereon"; "hereto"; "herewith"; "him"; "his"; "how"; "however"; "I"; "i.e."; "if"; "into"; "it"; "it's"; "its";
+        "me"; "more"; "most"; "mr"; "my"; "near"; "nor"; "now"; "of"; "onto"; "other"; "our"; "out"; "over"; "really"; 
+        "said"; "same"; "she"; "should"; "shouldn't"; "since"; "so"; "some"; "such"; "than"; "that"; "the"; "their"; 
+        "them"; "then"; "there"; "thereby"; "therefore"; "therefrom"; "therein"; "thereof"; "thereon"; "thereto"; 
+        "therewith"; "these"; "they"; "this"; "those"; "through"; "thus"; "to"; "too"; "under"; "until"; "unto"; "upon";
+        "us"; "very"; "viz"; "was"; "wasn't"; "we"; "were"; "what"; "when"; "where"; "whereby"; "wherein"; "whether";
+        "which"; "while"; "who"; "whom"; "whose"; "why"; "with"; "without"; "would"; "you"; "your" ; "have"; "thou"; "will"; 
+        "shall"
+    ]
+
+let lines = 
+    files
+    |> Array.map (fun f -> StoreClient.Default.CreateCloudArrayAsync("shakespeare", File.ReadLines(f)))
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Array.reduce (fun l r -> l.Append(r))
+
+[<Cloud>]
+let getTop count =
+    lines
+    |> CloudStream.ofCloudArray
+    |> CloudStream.flatMap (fun line -> 
+        line.Split([|' '; '.'; ','|], StringSplitOptions.RemoveEmptyEntries) 
+        |> Stream.ofArray
+        |> Stream.map (fun word -> word.ToLower())
+        |> Stream.map (fun word -> word.Trim()))
+    |> CloudStream.filter (fun word -> word.Length > 3 && not <| noiseWords.Contains(word))
+    |> CloudStream.countBy id
+    |> CloudStream.sortBy (fun (_,c) -> -c) count
+    |> CloudStream.toCloudArray
+
+let runtime = MBrace.InitLocal(totalNodes = 4)
+
+let proc = runtime.CreateProcess <@ getTop 20 @>
+
+proc.AwaitResult() |> Seq.iter (printfn "%A")
