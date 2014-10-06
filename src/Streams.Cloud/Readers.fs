5080d576af5adb8dc4f6fa005a27507839d81e78
krontogiannis <rodoconst@hotmail.com>
2014-10-06 16:07:29 +0300

    CloudFile readers

---
+++ src/Streams.Cloud/Readers.fs
@@ -0,0 +1,28 @@
+﻿namespace Nessos.Streams.Cloud
+    open System
+    open System.IO
+
+    [<AbstractClass; Sealed>]
+    type CloudFile =
+        static member ReadLines : Stream -> Async<string []> =
+            fun stream -> 
+                async { 
+                    return [| use sr = new StreamReader(stream)
+                              while not sr.EndOfStream do
+                                  yield sr.ReadLine() |]
+                }
+
+        static member ReadText : Stream -> Async<string> =
+            fun stream -> 
+                async { 
+                    use sr = new StreamReader(stream)
+                    return sr.ReadToEnd()
+                }
+
+        static member ReadBytes : Stream -> Async<byte []> =
+            fun stream -> 
+                async {
+                        use ms = new MemoryStream()
+                        do! Async.AwaitTask(stream.CopyToAsync(ms).ContinueWith(ignore))
+                        return ms.ToArray()
+                }
--- tests/Streams.Cloud.Tests/Script.fsx
+++ tests/Streams.Cloud.Tests/Script.fsx
@@ -18,28 +18,6 @@ let run (cloud : Cloud<'T>) =
     //MBrace.RunLocal cloud
 
 
-cloud { let! n = Cloud.GetWorkerCount() in return! [|1..n|] |> Array.map (fun _ -> cloud { return CloudArrayCache.State }) |> Cloud.Parallel }
-|> run
-|> Seq.iter (
-    fun state ->
-        printfn "-------------------------------------" 
-        state |> Seq.sort |> (Seq.iter (printfn "%A")))
-
-CloudArrayCache.State 
-|> Seq.sort
-|> Seq.iter (printfn "%A")
-
-cloud { let! n = Cloud.GetWorkerCount() in return! [|1..n|] |> Array.map (fun _ -> cloud { return CloudArrayCache.Guid }) |> Cloud.Parallel }
-|> run
-
-
-cloud { let! n = Cloud.GetWorkerCount() in return! [|1..n|] |> Array.map (fun _ -> cloud { return CloudArrayCache.Occupied }) |> Cloud.Parallel }
-|> run
-
-let cloudArray = StoreClient.Default.CreateCloudArray<int>("temp", Seq.empty) 
-let cached = CloudStream.cache cloudArray |> run
-
-CloudArrayCache.Occupied
 
 let ca' =
     cached
@@ -52,36 +30,29 @@ ca' |> Seq.toArray |> Seq.length
 
 
 
-open System.Collections.Generic
-open System.Collections.Concurrent
-
-let toSeq (enum : IEnumerator<_>)= 
-    seq {
-        while enum.MoveNext() do
-            yield enum.Current
-    }
-let getPartitions (totalWorkers : int) (s : int64) (e : int64) = 
-    let partitioner = Partitioner.Create(s, e)
-    let partitions = partitioner.GetPartitions(totalWorkers) 
-                        |> Seq.collect toSeq 
-                        |> Seq.toArray 
-    partitions
-
-
-getPartitions 8 0L 100L
-
-Partitioner.Create(0L,100L).GetPartitions(3)
-|> Seq.mapi (fun i x -> i, x |> toSeq )
-
-let xs = [1..2000]
-let f x 
-let p = Partitioner.Create(xs)
-
-let ps = p.GetPartitions(4)
-         |> Seq.toArray
-         |> Array.map (toSeq >> Seq.toArray)
 
 
 
 
-
+//cloud { let! n = Cloud.GetWorkerCount() in return! [|1..n|] |> Array.map (fun _ -> cloud { return CloudArrayCache.State }) |> Cloud.Parallel }
+//|> run
+//|> Seq.iter (
+//    fun state ->
+//        printfn "-------------------------------------" 
+//        state |> Seq.sort |> (Seq.iter (printfn "%A")))
+//
+//CloudArrayCache.State 
+//|> Seq.sort
+//|> Seq.iter (printfn "%A")
+//
+//cloud { let! n = Cloud.GetWorkerCount() in return! [|1..n|] |> Array.map (fun _ -> cloud { return CloudArrayCache.Guid }) |> Cloud.Parallel }
+//|> run
+//
+//
+//cloud { let! n = Cloud.GetWorkerCount() in return! [|1..n|] |> Array.map (fun _ -> cloud { return CloudArrayCache.Occupied }) |> Cloud.Parallel }
+//|> run
+//
+//let cloudArray = StoreClient.Default.CreateCloudArray<int>("temp", Seq.empty) 
+//let cached = CloudStream.cache cloudArray |> run
+//
+//CloudArrayCache.Occupied
