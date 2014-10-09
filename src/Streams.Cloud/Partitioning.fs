034dfff5cd412131c31f57b2ac6d30ed90a47494
krontogiannis <rodoconst@hotmail.com>
2014-10-09 12:55:21 +0300

    ofRange refactoring

--- src/Streams.Cloud/Partitioning.fs
+++ src/Streams.Cloud/Partitioning.fs
@@ -4,7 +4,7 @@ module internal Partitions =
     open System.Collections.Generic
     open System.Collections.Concurrent
 
-    let ofLongRange (totalWorkers : int) (s : int64) (e : int64) = 
+    let ofLongRange (totalWorkers : int) (s : int64) (e : int64) : (int64 * int64) []  = 
         let toSeq (enum : IEnumerator<_>)= 
             seq {
                 while enum.MoveNext() do
@@ -16,17 +16,9 @@ module internal Partitions =
                          |> Seq.toArray 
         partitions
 
-    let ofRange (totalWorkers : int) (s : int) (e : int) = 
-        let toSeq (enum : IEnumerator<_>)= 
-            seq {
-                while enum.MoveNext() do
-                    yield enum.Current
-            }
-        let partitioner = Partitioner.Create(s, e)
-        let partitions = partitioner.GetPartitions(totalWorkers) 
-                         |> Seq.collect toSeq 
-                         |> Seq.toArray 
-        partitions
+    let ofRange (totalWorkers : int) (s : int) (e : int) : (int * int) [] = 
+        ofLongRange totalWorkers (int64 s) (int64 e)
+        |> Array.map (fun (s,e) -> int s, int e)
 
     let ofArray (totalWorkers : int) (array : 'T []) : 'T [] [] =
         let ranges = ofRange totalWorkers 0 array.Length
