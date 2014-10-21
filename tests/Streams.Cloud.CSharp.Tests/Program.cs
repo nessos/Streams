b0a04ba59403405b062a1748dd8425a66b13bf8a
krontogiannis <rodoconst@hotmail.com>
2014-10-21 15:56:35 +0300

    Added cloud C# test proj

---
+++ tests/Streams.Cloud.CSharp.Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams.Cloud.CSharp.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
