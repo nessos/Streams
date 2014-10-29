44225d4aa8bcfe48dd2735712dcbe2161b7878e3
krontogiannis <rodoconst@hotmail.com>
2014-10-29 16:16:25 +0200

    Packaging and version cleanup

--- RELEASE_NOTES_CLOUDSTREAM.md
+++ RELEASE_NOTES_CLOUDSTREAM.md
@@ -1,5 +1,6 @@
 #### 0.2.0-alpha
 * API changes and improvements.
+* C# support.
 
 #### 0.1.0-alpha
 * Initial Release.
--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -17,8 +17,9 @@ A lightweight F#/C# library for efficient functional-style pipelines on streams 
   <div class="span6">
     <div class="well well-small" id="nuget">
       Install via <a href="https://nuget.org/packages/Streams">NuGet</a>:
-      <pre>PM> Install-Package Streams</pre>
-      <pre>PM> Install-Package Streams.CSharp</pre>
+      <pre>PM> Install-Package Streams
+PM> Install-Package Streams.CSharp
+PM> Install-Package Streams.Cloud -Pre</pre>
     </div>
   </div>
   <div class="span1"></div>
