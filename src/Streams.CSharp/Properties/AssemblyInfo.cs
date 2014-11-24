97dfab18c1a2743926b7658e9f962c4196a64575
npal <nick.palladinos@gmail.com>
2014-11-24 17:10:49 +0200

    New versions 0.2.6

--- src/Streams.CSharp/Properties/AssemblyInfo.cs
+++ src/Streams.CSharp/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Streams")]
 [assembly: AssemblyProductAttribute("Streams")]
 [assembly: AssemblyCompanyAttribute("Nessos Information Technologies")]
-[assembly: AssemblyVersionAttribute("0.2.5")]
-[assembly: AssemblyFileVersionAttribute("0.2.5")]
+[assembly: AssemblyVersionAttribute("0.2.6")]
+[assembly: AssemblyFileVersionAttribute("0.2.6")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.2.5";
+        internal const string Version = "0.2.6";
     }
 }
